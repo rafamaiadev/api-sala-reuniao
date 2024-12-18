5bc0269f91e3e81fc3e56654cb2d9fc15d5636a4
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2024-12-18 20:19:38 -0300

    first commit

---
+++ src/main/java/com/rafamaiadev/api_sala_reuniao/ApiSalaReuniaoApplication.java
@@ -0,0 +1,13 @@
+package com.rafamaiadev.api_sala_reuniao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSalaReuniaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSalaReuniaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafamaiadev/api_sala_reuniao/ApiSalaReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafamaiadev.api_sala_reuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSalaReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
